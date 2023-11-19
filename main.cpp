694fb0ba776848eb459d7977d7854b4e836ffed7
ToKiNoBug <tokinobug@163.com>
2023-11-19 13:33:10 +0800

    add libzip dependency

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <QMainWindow>
 #include <QApplication>
+#include <zip.h>
+#include <iostream>
 
 int main(int argc,char**argv) {
     QApplication app{argc,argv};
 
     QMainWindow w;
     w.show();
+
+    std::cout<<"Version of libzip: "<<zip_libzip_version()<<std::endl;
 
     return app.exec();
 }
